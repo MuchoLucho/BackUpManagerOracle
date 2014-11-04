c6c3ec501e34940f289c508a1f02263e36768572
Jota Porras <jporras06@gmail.com>
2014-11-03 21:54:54 -0600

    Create NARFDIR

---
+++ Ejecutor/src/PACKAGES/NARFDirs.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PACKAGES;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.sql.DriverManager;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author jota
+ */
+public class NARFDirs {//All the directories and files needed for the software to work. Files must always end with /
+
+    public static final String home = System.getProperty("user.home");
+    public static final String strats = home + "/narf/executor/strategies/";
+    public static final String scripts = home + "/narf/executor/rman_scripts/";
+    public static final String logs = home + "/narf/revisor/logs/";
+    public static final String backups = home + "/narf/executor/backup_files/";
+    public static final String configFile = home + "/narf/executor/config";
+
+    /*MOTHERSHIP (The instance running Constructor)*/
+    public static String motherDBLink = "luisk";
+    public static String motherUser = "luisk";
+    public static String motherIP = "127.0.0.1";
+    public static String motherLogs = "~/narf/revisor/logs/";//never changes
+
+    /*MY DB*/
+    public static String dbusername = "sys as sysdba";
+    public static String dbpassword = "root";
+    public static String dbport = "1521";
+    public static String dbInstance = "XE";
+
+    public static void setParameters() {
+        File configFile = new File(NARFDirs.configFile);
+        String mUser = null;
+        String mIP = null;
+        try {
+            //LIST OF ALL CONFIG LINES.
+            ArrayList<String> configLines = (ArrayList<String>) Files.lines(configFile.toPath()).collect(Collectors.toList()); //In case I need any more parameters
+            //Reading First line: Mother parameters
+            NARFDirs.setMotherShipParameters(configLines.get(0));
+            //Reading Second line, Database parameters
+            NARFDirs.setDBParameters(configLines.get(1));
+
+        } catch (IOException ex) {
+            System.err.println("CONFIG FILE FAILURE.");
+            Logger.getLogger(NARFDirs.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        if (motherUser != null && mIP != null) {
+            NARFDirs.motherUser = mUser;
+            NARFDirs.motherIP = mIP;
+        }
+
+    }
+
+    
+    
+    private static void setMotherShipParameters(String msParamLine) {
+        String[] motherParameters = null;
+        if (msParamLine != null && !msParamLine.isEmpty()) {
+            motherParameters = msParamLine.split("\\t");
+        }
+         /*
+             MOTHERLODE
+             [0] database link
+             [1] IP
+             [2] Linux Username
+             */
+        if (motherParameters != null && motherParameters.length == 3) {
+            motherDBLink = motherParameters[0];
+            motherIP = motherParameters[1];
+            motherUser = motherParameters[2];
+        } else {
+            System.err.println("COULD NOT SET MOTHER PARAMETERS, USING DEFAULT");
+        }
+    }
+
+    private static void setDBParameters(String dbParamLine) {
+        String[] myDBParameters = null;
+        if (dbParamLine != null && !dbParamLine.isEmpty()) {
+            myDBParameters = dbParamLine.split("\\t");
+        }
+        /*
+         Database parameters
+         [0] username
+         [1] password
+         [2] port
+         [3] Instance
+         */
+        if (myDBParameters != null && myDBParameters.length == 4) {
+            dbusername = myDBParameters[0];
+            dbpassword = myDBParameters[1];
+            dbport = myDBParameters[2];
+            dbInstance = myDBParameters[3];
+        }
+    }
+
+}
